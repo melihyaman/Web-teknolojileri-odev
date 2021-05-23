9232a3b1b0504e0732de23138f2ec3853646d9e4
melihyaman <83470990+melihyaman@users.noreply.github.com>
2021-05-23 16:57:42 +0300

    güncelleme

--- sayfalar/index.php
+++ sayfalar/index.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="Css/style.css">
+    <script src="https://www.google.com/recaptcha/api.js?hl=tr"></script>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
+    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
     <title>Giriş Yap</title>
 </head>
 
@@ -17,26 +20,37 @@
                     <div class="form-group">
                         <label for="kullaniciadi">Kullanıcı Adı</label>
                         <input type="text" class="form-control" id="userId" name="kullaniciadi" placeholder="Kullanıcı Adı">
-
+                        
                     </div>
                     <div class="form-group">
                         <label for="sifre">Password</label>
                         <input type="password" class="form-control" id="password" name="sifre" placeholder="Şifre">
-                    
-                        <a href="/index.html">git</a>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Girş Yap</button>
                 </form>
             </div>
         </div>
     </div>
+    <script type='text/javascript'>alert("HOŞGELDİNİZ")</script>
+
 </body>
+
 </html>
+
+
+
+
+
+
+
+
 
 <?php
 $kullaniciadi = "admin";
 $sifre = "12345";
 if (isset($_POST['kullaniciadi'])) {
-    if ($_POST['kullaniciadi'] == $kullaniciadi && $_POST['sifre'] == $sifre && $_POST['captcha'] == true) {
-        header("Location:Veritabani.php");
+    if ($_POST['kullaniciadi'] == $kullaniciadi && $_POST['sifre'] == $sifre) {
+        header("Location:ana-sayfa.html");
     } else {
         echo "<script type='text/javascript'>alert('Hatalı kullanıcı adı/şifre');</script>";
     }
